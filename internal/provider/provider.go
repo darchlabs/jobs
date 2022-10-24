09895f5c7d8c57cb85bbcf9dfa0ce2819de8b3aa
NicoBarragan <nbarragan01@gmail.com>
2022-10-24 01:31:38 -0300

    refactor(provider.go): updated struct for interface

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,15 +1,9 @@
 package provider
 
-type Provider struct {
-	Name         string
-	NeedsFunding bool
-	Active       bool
-	IsSetup      bool
-	IsWorking    bool
-
-	Setup func() bool
-	Fund  func(int64) bool
+type Provider interface {
+	Setup() bool
+	Fund(int64) bool
 
 	/// @dev: GetState(Name) (IsWorking, IsSetup, NeedsFunding)
-	GetState func(string) (bool, bool, bool)
+	GetState(string) (bool, bool, bool)
 }
