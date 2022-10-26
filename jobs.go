aa76c3f3c92f8cfe1ae93c21ef5ea117f85f5230
nicobarra <nbarragan01@gmail.com>
2022-10-26 02:15:26 -0300

    refactor(jobs.go): renamed interface

--- jobs.go
+++ jobs.go
@@ -2,15 +2,18 @@ package jobs
 
 import prov "github.com/darchlabs/jobs/internal/provider"
 
-type ImplementationStorage interface {
+type ProviderStorage interface {
 	ListImplementations() []*prov.Provider
 	GetImplementation(implementationName string) (*prov.Provider, error)
+
+	/* TODO(nb): Don't know how to add the new imported repo, if post the url to the repo by an API or import in repo and update manually in DB
+	 * Maybe, add a hook for when a new implementation is added via API, it is inserted in the DB */
 	AddImplementation(implementation *prov.Provider) error
 	UpdateImplementation(implementation *prov.Provider) error
 	DeleteImplementation(implementation *prov.Provider) error
 }
 
-type UserJobsStorage interface {
+type JobStorage interface {
 	GetUsingImplementations(userId string) []*prov.Provider
 	GetCurrentContracts(userId string) []string
 }
