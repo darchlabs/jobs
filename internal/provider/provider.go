57cf92b20e28f9841eb2ed9b6dd1fe58fa6259ed
nicobarra <nbarragan01@gmail.com>
2022-10-26 02:14:01 -0300

    refactor(provider.go): renamed interface and moved Provider struct for DB t this file

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,6 +1,14 @@
 package provider
 
-type Provider interface {
+// Struct for DB
+type Provider struct {
+	Id            string
+	Name          string
+	Implemetation Implementation
+}
+
+// Interface required for each new implementation
+type Implementation interface {
 	Setup() bool
 	Fund(amount int64) bool
 	GetState(name string) (IsWorking bool, IsSetup bool, NeedsFunding bool)
