8df7391899b86959fa5fbddd67cf590fadc6bc12
nicobarra <nbarragan01@gmail.com>
2022-10-27 12:55:08 -0300

    refactor(provider.go): updated id from int to uint and updated id incremental logic

--- internal/storage/provider/provider.go
+++ internal/storage/provider/provider.go
@@ -22,14 +22,14 @@ func New(s *storage.S) *PS {
 // TODO(nb): add authorization methods only for Darch Labs team IN HANDLERS
 
 func (ps *PS) AddImplementation(p *provider.Provider) error {
+	var id uint8
 	// Validate the implementation received is the correct one
 	implType := fmt.Sprintf("%T", p.Implemetation)
 	if implType != "provider.Implementation" {
 		return fmt.Errorf("%s", "The implementation doesn't fit the Provider interface required")
 	}
 
-	// Validate if the provider already exists and get last id
-	var id int8
+	// Validate if the provider already exists
 	iter := ps.storage.DB.NewIterator(nil, nil)
 	for iter.Next() {
 		var pprovider *provider.Provider
@@ -43,10 +43,6 @@ func (ps *PS) AddImplementation(p *provider.Provider) error {
 			return fmt.Errorf("%s", "This provider already exists")
 		}
 
-		// TODO(nb): iteration over leveldb respects the order? The last item iterated is indeed the last inserted?
-		// Get the last id
-		id = pprovider.Id
-
 	}
 	iter.Release()
 
@@ -56,7 +52,8 @@ func (ps *PS) AddImplementation(p *provider.Provider) error {
 	}
 
 	// add 1 to the last id in order to make it incremental
-	p.Id = id + 1
+	id += 1
+	p.Id = id
 
 	// JSON.stringify
 	b, err := json.Marshal(p)
@@ -99,7 +96,7 @@ func (ps *PS) ListImplementations() []*provider.Provider {
 	return providers
 }
 
-func (ps *PS) GetImplementation(id int8) (*provider.Provider, error) {
+func (ps *PS) GetImplementation(id uint8) (*provider.Provider, error) {
 	// Get the provider by the id key in bytes
 	data, err := ps.storage.DB.Get([]byte(fmt.Sprintf("%d", id)), nil)
 	if err != nil {
@@ -117,7 +114,7 @@ func (ps *PS) GetImplementation(id int8) (*provider.Provider, error) {
 	return p, nil
 }
 
-func (ps *PS) UpdateImplementation(id int8, p *provider.Provider) error {
+func (ps *PS) UpdateImplementation(id uint8, p *provider.Provider) error {
 	// Validate the implementation exists
 	data, err := ps.storage.DB.Get([]byte(fmt.Sprintf("%d", id)), nil)
 	if err != nil {
@@ -149,7 +146,7 @@ func (ps *PS) UpdateImplementation(id int8, p *provider.Provider) error {
 	return nil
 }
 
-func (ps *PS) DeleteImplementation(id int8) error {
+func (ps *PS) DeleteImplementation(id uint8) error {
 	// Delete provider from DB
 	err := ps.storage.DB.Delete([]byte(fmt.Sprintf("%d", id)), nil)
 	if err != nil {
