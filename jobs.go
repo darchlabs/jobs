50fd50e8d817037e7e63bcac0bd6cd724a7132be
nicobarra <nbarragan01@gmail.com>
2022-10-27 13:25:47 -0300

    refactor(jobs.go): updated id struct's param from int to uint

--- jobs.go
+++ jobs.go
@@ -4,13 +4,13 @@ import prov "github.com/darchlabs/jobs/internal/provider"
 
 type ProviderStorage interface {
 	ListImplementations() []*prov.Provider
-	GetImplementation(id int8) (*prov.Provider, error)
+	GetImplementation(id uint8) (*prov.Provider, error)
 
 	/* TODO(nb): Don't know how to add the new imported repo, if post the url to the repo by an API or import in repo and update manually in DB
 	 * Maybe, add a hook for when a new implementation is added via API, it is inserted in the DB */
 	AddImplementation(implementation *prov.Provider) error
-	UpdateImplementation(id int8, implementation *prov.Provider) error
-	DeleteImplementation(id int8) error
+	UpdateImplementation(id uint8, implementation *prov.Provider) error
+	DeleteImplementation(id uint8) error
 }
 
 type JobStorage interface {
