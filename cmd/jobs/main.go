cf42f7038a5f4367191e93f5f71ebfb604d82fd3
nicobarra <nbarragan01@gmail.com>
2022-10-28 17:12:42 -0300

    feat(main.go): created main func that initializes db, gets env values and executes the api

--- cmd/jobs/main.go
+++ cmd/jobs/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/darchlabs/jobs/internal/api/providers"
+	providers "github.com/darchlabs/jobs/internal/providers/route"
+	"github.com/darchlabs/jobs/internal/storage"
+	providerstorage "github.com/darchlabs/jobs/internal/storage/provider"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Jobs module being created...")
+	// Get and validate env values
+	dbPath := os.Getenv("PATH")
+	if dbPath == "" {
+		log.Fatal("Invalid DB filepath")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Invalid port")
+	}
+
+	// Initialize storage
+	s, err := storage.New(dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initialize provider storage
+	ps := providerstorage.New(s)
+
+	// Initialize fiber
+	api := fiber.New()
+
+	// Configure routers
+	providers.Route(api, providers.Context{ProviderStorage: *ps})
 }
