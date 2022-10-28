2638937fcb7fbdd640ba247799baa310c72d3018
nicobarra <nbarragan01@gmail.com>
2022-10-28 19:42:48 -0300

    feat(route.go): created ctx and route with endpoints for jobsapi

--- internal/api/jobs/route.go
+++ internal/api/jobs/route.go
@@ -1,1 +1,15 @@
-package jobs
+package jobsapi
+
+import (
+	jobstorage "github.com/darchlabs/jobs/internal/storage/job"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Context struct {
+	JobStorage jobstorage.JS
+}
+
+func Route(app *fiber.App, ctx Context) {
+	app.Get("/api/v1/jobs", listJobsHandler(ctx))
+	// app.Post("/api/v1/jobs", createProviderHandler(ctx))
+}
