1f350f6438e978a25da8aa0463b088fa1ae02f55
nicobarra <nbarragan01@gmail.com>
2022-10-31 13:10:03 -0300

    feat(create-jobs-handler.go): implemented handler that validates, uses the insert func and returns a res

--- internal/api/jobs/create-jobs-handler.go
+++ internal/api/jobs/create-jobs-handler.go
@@ -1,7 +1,44 @@
 package jobsapi
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
 
-func createJobsHandler(ctx Context) func(c *fiber.Ctx) error {
-	return nil
+	"github.com/darchlabs/jobs/internal/job"
+	jobstorage "github.com/darchlabs/jobs/internal/storage/job"
+	"github.com/go-playground/validator"
+)
+
+type CreateJobsHandler struct {
+	storage jobstorage.JS
+}
+
+func NewCreateJobsHandler(js jobstorage.JS) *CreateJobsHandler {
+	return &CreateJobsHandler{
+		storage: js,
+	}
+}
+
+func (CreateJobsHandler) Invoke(ctx Context) *HandlerRes {
+	// Prepare body request struct
+	body := struct {
+		Job *job.Job `json:"job"`
+	}{}
+
+	// Parse body to Job struct
+	err := json.Unmarshal(ctx.c.Body(), &body)
+	if err != nil {
+		return &HandlerRes{err.Error(), 500, err}
+	}
+
+	// TODO(nb): validate
+	validate := validator.New()
+	validate.Struct(ctx.JobStorage)
+
+	// Insert job in jobstorage DB
+	j, err := ctx.JobStorage.Insert(body.Job)
+	if err != nil {
+		return &HandlerRes{err.Error(), 500, err}
+	}
+
+	return &HandlerRes{j, 200, nil}
 }
