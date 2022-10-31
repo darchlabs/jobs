73689e477258ef65921446bad550c64061104736
nicobarra <nbarragan01@gmail.com>
2022-10-31 13:08:27 -0300

    feat(route.go): added fiber.c in Context struct for after getting body params

--- internal/api/jobs/route.go
+++ internal/api/jobs/route.go
@@ -8,8 +8,8 @@ import (
 
 type Context struct {
 	JobStorage jobstorage.JS
+	c          *fiber.Ctx
 }
-
 
 // TODO(nb): Should I put this handler definitions in `api.go` file? Because they're repeated
 // in both router files. The same question goes for `handlerFunc` method
@@ -27,7 +27,7 @@ type handler func(ctx Context) *HandlerRes
 func Route(app *fiber.App, ctx Context) {
 	listJobsHandler := NewListJobsHandler(ctx.JobStorage)
 	createJobsHandler := NewCreateJobsHandler(ctx.JobStorage)
-	
+
 	app.Get("/api/v1/jobs", HandleFunc(listJobsHandler.Invoke, ctx))
 	app.Post("/api/v1/jobs", HandleFunc(createJobsHandler.Invoke, ctx))
 }
@@ -37,8 +37,8 @@ func HandleFunc(fn handler, ctx Context) func(c *fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
 		// set headers
 		c.Accepts("application/json")
-
 		// Exec handler func and get its response
+		ctx.c = c
 		handlerRes := fn(ctx)
 		payload, statusCode, err := handlerRes.Payload, handlerRes.HttpStatus, handlerRes.err
 		if err != nil {
@@ -57,5 +57,4 @@ func HandleFunc(fn handler, ctx Context) func(c *fiber.Ctx) error {
 		// Return response
 		return c.Status(fiber.StatusOK).JSON(res)
 	}
->>>>>>> nb-feat/list-providers-endpoint
 }
