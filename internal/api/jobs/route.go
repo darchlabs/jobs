6ef08e82f72817fd50fe2f0f3b863b308397a05f
nicobarra <nbarragan01@gmail.com>
2022-11-01 11:39:52 -0300

    feat(route.go): deleted response struct, updated var and imported struct names

--- internal/api/jobs/route.go
+++ internal/api/jobs/route.go
@@ -2,27 +2,17 @@ package jobsapi
 
 import (
 	"github.com/darchlabs/jobs/internal/api"
-	jobstorage "github.com/darchlabs/jobs/internal/storage/job"
+	"github.com/darchlabs/jobs/internal/storage"
 	"github.com/gofiber/fiber/v2"
 )
 
 type Context struct {
-	JobStorage jobstorage.JS
+	JobStorage storage.Job
 	c          *fiber.Ctx
 }
 
-// TODO(nb): Should I put this handler definitions in `api.go` file? Because they're repeated
-// in both router files. The same question goes for `handlerFunc` method
-
-// handler's response
-type HandlerRes struct {
-	Payload    interface{}
-	HttpStatus int
-	err        error
-}
-
 // Define handler method
-type handler func(ctx Context) *HandlerRes
+type handler func(ctx Context) *api.HandlerRes
 
 func Route(app *fiber.App, ctx Context) {
 	listJobsHandler := NewListJobsHandler(ctx.JobStorage)
@@ -40,11 +30,10 @@ func HandleFunc(fn handler, ctx Context) func(c *fiber.Ctx) error {
 		// Exec handler func and get its response
 		ctx.c = c
 		handlerRes := fn(ctx)
-		payload, statusCode, err := handlerRes.Payload, handlerRes.HttpStatus, handlerRes.err
+		payload, statusCode, err := handlerRes.Payload, handlerRes.HttpStatus, handlerRes.Err
 		if err != nil {
 			return c.Status(fiber.StatusConflict).JSON(api.Response{
 				Error: err,
-				Data:  payload,
 			})
 		}
 
