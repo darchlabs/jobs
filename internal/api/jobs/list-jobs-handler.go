85de427f17b055d10fc4408fbb66429cb5d13b00
nicobarra <nbarragan01@gmail.com>
2022-11-01 11:38:47 -0300

    refactor(list-jobs-handler.go): updated struct and variable names

--- internal/api/jobs/list-jobs-handler.go
+++ internal/api/jobs/list-jobs-handler.go
@@ -1,26 +1,27 @@
 package jobsapi
 
 import (
-	jobstorage "github.com/darchlabs/jobs/internal/storage/job"
+	"github.com/darchlabs/jobs/internal/api"
+	"github.com/darchlabs/jobs/internal/storage"
 )
 
 type ListJobsHandler struct {
-	storage jobstorage.JS
+	storage storage.Job
 }
 
-func NewListJobsHandler(js jobstorage.JS) *ListJobsHandler {
+func NewListJobsHandler(js storage.Job) *ListJobsHandler {
 	return &ListJobsHandler{
 		storage: js,
 	}
 }
 
-func (ListJobsHandler) Invoke(ctx Context) *HandlerRes {
+func (ListJobsHandler) Invoke(ctx Context) *api.HandlerRes {
 	// Get elements from db
 	data, err := ctx.JobStorage.List()
 	if err != nil {
-		return &HandlerRes{err.Error(), 500, err}
+		return &api.HandlerRes{Payload: err.Error(), HttpStatus: 500, Err: err}
 	}
 
 	// Prepare response
-	return &HandlerRes{data, 200, nil}
+	return &api.HandlerRes{Payload: data, HttpStatus: 200, Err: nil}
 }
