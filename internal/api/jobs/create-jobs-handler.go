722167dbd79067b447b478b82e5d268deb33ede6
nicobarra <nbarragan01@gmail.com>
2022-11-01 11:39:12 -0300

    refactor(create-jobs-handler.go): updated struct and variable names

--- internal/api/jobs/create-jobs-handler.go
+++ internal/api/jobs/create-jobs-handler.go
@@ -3,22 +3,23 @@ package jobsapi
 import (
 	"encoding/json"
 
+	"github.com/darchlabs/jobs/internal/api"
 	"github.com/darchlabs/jobs/internal/job"
-	jobstorage "github.com/darchlabs/jobs/internal/storage/job"
+	"github.com/darchlabs/jobs/internal/storage"
 	"github.com/go-playground/validator"
 )
 
 type CreateJobsHandler struct {
-	storage jobstorage.JS
+	storage storage.Job
 }
 
-func NewCreateJobsHandler(js jobstorage.JS) *CreateJobsHandler {
+func NewCreateJobsHandler(js storage.Job) *CreateJobsHandler {
 	return &CreateJobsHandler{
 		storage: js,
 	}
 }
 
-func (CreateJobsHandler) Invoke(ctx Context) *HandlerRes {
+func (CreateJobsHandler) Invoke(ctx Context) *api.HandlerRes {
 	// Prepare body request struct
 	body := struct {
 		Job *job.Job `json:"job"`
@@ -27,18 +28,17 @@ func (CreateJobsHandler) Invoke(ctx Context) *HandlerRes {
 	// Parse body to Job struct
 	err := json.Unmarshal(ctx.c.Body(), &body)
 	if err != nil {
-		return &HandlerRes{err.Error(), 500, err}
+		return &api.HandlerRes{Payload: err.Error(), HttpStatus: 500, Err: err}
 	}
 
-	// TODO(nb): validate
 	validate := validator.New()
 	validate.Struct(ctx.JobStorage)
 
 	// Insert job in jobstorage DB
 	j, err := ctx.JobStorage.Insert(body.Job)
 	if err != nil {
-		return &HandlerRes{err.Error(), 500, err}
+		return &api.HandlerRes{Payload: err.Error(), HttpStatus: 500, Err: err}
 	}
 
-	return &HandlerRes{j, 200, nil}
+	return &api.HandlerRes{Payload: j, HttpStatus: 200, Err: nil}
 }
