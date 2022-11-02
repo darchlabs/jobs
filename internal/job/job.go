ff90381abc1f5b0492d8fece914a8c4af7ed4620
nicobarra <nbarragan01@gmail.com>
2022-11-02 19:15:02 -0300

    fix(job.go): fixed error in UpdatedAt field name

--- internal/job/job.go
+++ internal/job/job.go
@@ -3,19 +3,17 @@ package job
 import "time"
 
 type Job struct {
-	ID           string     `json:"id,omitempty"`
-	Name         string     `json:"name"`
-	ProviderId   string     `json:"providerId"`
-	Status       string     `json:"status,omitempty"`
-	Network      string     `json:"network"`
-	Address      string     `json:"address"`
-	Abi          string     `json:"abi"`
-	Type         string     `json:"type"`
-	Cronjob      string     `json:"cronjob,omitempty"`
-	CheckMethod  *string    `json:"checkMethod"`
-	ActionMethod string     `json:"actionMethod"`
-	CreatedAt    time.Time  `json:"createdAt,omitempty"`
-	UpddatedAt   *time.Time `json:"updatedAt,omitempty"`
+	ID           string    `json:"id,omitempty"`
+	Name         string    `json:"name"`
+	ProviderId   string    `json:"providerId"`
+	Status       string    `json:"status,omitempty"`
+	Network      string    `json:"network"`
+	Address      string    `json:"address"`
+	Abi          string    `json:"abi"`
+	Type         string    `json:"type"`
+	Cronjob      string    `json:"cronjob,omitempty"`
+	CheckMethod  *string   `json:"checkMethod"`
+	ActionMethod string    `json:"actionMethod"`
+	CreatedAt    time.Time `json:"createdAt,omitempty"`
+	UpdatedAt    time.Time `json:"updatedAt,omitempty"`
 }
-
-// func (j *Job) UpdateStatus()
