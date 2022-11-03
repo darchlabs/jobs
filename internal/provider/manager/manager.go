e7f60aad801598f0829919022af0ed64b6afef68
nicobarra <nbarragan01@gmail.com>
2022-11-03 02:14:04 -0300

    feat(manager.go): updated struct, constructor logic, and added an update to the db when sthing fails inside cronjob

--- internal/provider/manager/manager.go
+++ internal/provider/manager/manager.go
@@ -1,3 +1,5 @@
+/* Darch Labs implementation for Keepers */
+
 package providermanager
 
 import (
@@ -6,6 +8,7 @@ import (
 
 	"github.com/darchlabs/jobs/internal/job"
 	"github.com/darchlabs/jobs/internal/storage"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/robfig/cron"
 )
 
@@ -14,11 +17,13 @@ type Manager interface {
 }
 
 type M struct {
-	jobs []*job.Job
-	cron *cron.Cron
+	jobstorage *storage.Job
+	cron       *cron.Cron
+	client     *ethclient.Client
+	privateKey string
 }
 
-func NewManager(js *storage.Job) *M {
+func NewManager(js *storage.Job, client *ethclient.Client, pk string) *M {
 	// get jobs from db
 	currentJobs, err := js.List()
 	if err != nil {
@@ -27,8 +32,10 @@ func NewManager(js *storage.Job) *M {
 	}
 
 	m := &M{
-		jobs: make([]*job.Job, 0),
-		cron: cron.New(),
+		jobstorage: js,
+		cron:       cron.New(),
+		client:     client,
+		privateKey: pk,
 	}
 
 	// Iterate jobs and create them for if there were jobs running, sthing failed and is needed to be reloaded
@@ -40,7 +47,6 @@ func NewManager(js *storage.Job) *M {
 }
 
 func (m *M) Create(j *job.Job) error {
-	m.jobs = append(m.jobs, j)
 	var err error
 
 	if j.Type != "cronjob" && j.Type != "synchronizer" {
@@ -58,28 +64,64 @@ func (m *M) Create(j *job.Job) error {
 }
 
 func (m *M) createCronjob(j *job.Job) error {
+	var err error
 	fmt.Println("Entered")
-	m.cron.AddFunc(j.Cronjob, func() {
+	err = m.cron.AddFunc(j.Cronjob, func() {
+		execute := true
+
 		fmt.Println("I'm here!")
 
-		// Get provider and evaluate it
+		// Get blockchain id
+		chainId := getChainId(j.Network)
+		if chainId == int64(0) {
+			err = fmt.Errorf("invalid chain id for %s network", j.Network)
+			m.stopCronjob(j)
+		}
 
-		// Get instance of the contract w address (and abi)
+		// Get signer for then execute the tx and evaluate it
+		signer, err := getSigner(m.privateKey, *m.client, chainId, nil, nil)
+		if err != nil {
+			m.stopCronjob(j)
+		}
+
+		// TODO(nb): Get instance of the contract w address (and abi)
 
 		// check if j.CheckMethod is nil. If it is nil execute action methdo directly.
-		// If not, execute action method and see return
+		if j.CheckMethod != nil {
+			// TODO(nb): Execute smart contract view method
+			execute = false
+		}
+
+		// Execute action method and see return
+		if execute && err == nil {
+			fmt.Print("1")
+			// TODO(nb): Execute smart contract method
+		}
 
 		// TODO(nb): add state or chan for running different process
 
-		// TODO(nb): if the status changes (e.g error) the BD needs to be updated. Decide where & how
+		if err != nil {
+			m.stopCronjob(j)
+		}
 	})
-	m.cron.Start()
 
+	if err != nil {
+		return err
+	}
+
+	m.cron.Start()
 	return nil
 }
 
-// Implementation when a listener over events is needed (synchronizer) --> V2
+// Method for stopping cronjob when an error is occurred
+func (m *M) stopCronjob(j *job.Job) {
+	j.Status = "error"
+	m.jobstorage.Update(j)
+	m.cron.Stop()
+}
+
+// Implementation when a listener over events is needed (synchronizer) --> Jobs V2
 func (m *M) createSynchronizer(j *job.Job) error {
-	// TODO(nb): create syncrhronizer code for listening to an event
+	// TODO(nb): V2 create syncrhronizer code for listening to an event
 	return nil
 }
