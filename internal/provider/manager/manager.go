22d07ceac63b9049f7410b0303297760589e4b45
nicobarra <nbarragan01@gmail.com>
2022-11-08 11:49:14 -0300

    feat(manager.go): created checkAndStop func and logic for interacting w contracts using token file funcs

--- internal/provider/manager/manager.go
+++ internal/provider/manager/manager.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+
 	"github.com/darchlabs/jobs/internal/job"
 	"github.com/darchlabs/jobs/internal/storage"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/robfig/cron"
 )
 
@@ -64,9 +67,8 @@ func (m *M) Create(j *job.Job) error {
 }
 
 func (m *M) createCronjob(j *job.Job) error {
-	var err error
 	fmt.Println("Entered")
-	err = m.cron.AddFunc(j.Cronjob, func() {
+	err := m.cron.AddFunc(j.Cronjob, func() {
 		execute := true
 
 		fmt.Println("I'm here!")
@@ -74,35 +76,43 @@ func (m *M) createCronjob(j *job.Job) error {
 		// Get blockchain id
 		chainId := getChainId(j.Network)
 		if chainId == int64(0) {
-			err = fmt.Errorf("invalid chain id for %s network", j.Network)
-			m.stopCronjob(j)
+			err := fmt.Errorf("invalid chain id for %s network", j.Network)
+			m.checkAndStop(err, j)
 		}
 
 		// Get signer for then execute the tx and evaluate it
 		signer, err := getSigner(m.privateKey, *m.client, chainId, nil, nil)
-		if err != nil {
-			m.stopCronjob(j)
-		}
+		m.checkAndStop(err, j)
 
-		// TODO(nb): Get instance of the contract w address (and abi)
+		// Parse address
+		address := common.HexToAddress(j.Address)
+
+		// Get an instance of the smart contract
+		contract, err := NewToken(address, j.Abi, m.client)
+		m.checkAndStop(err, j)
 
 		// check if j.CheckMethod is nil. If it is nil execute action methdo directly.
 		if j.CheckMethod != nil {
-			// TODO(nb): Execute smart contract view method
-			execute = false
+			// Create results interface array for appending the result of the sc view function on it
+			results := make([]interface{}, 0)
+
+			// Call the view function and get its boolean response
+			err = contract.contractCaller.Call(&bind.CallOpts{}, &results, *j.CheckMethod, nil)
+			m.checkAndStop(err, j)
+
+			execute = results[0] == true
 		}
 
 		// Execute action method and see return
 		if execute && err == nil {
 			fmt.Print("1")
-			// TODO(nb): Execute smart contract method
+			tx, err := contract.contractTransactor.Transact(&bind.TransactOpts{From: signer.From}, j.ActionMethod, nil)
+
+			m.checkAndStop(err, j)
+			fmt.Println("tx: ", tx)
 		}
 
 		// TODO(nb): add state or chan for running different process
-
-		if err != nil {
-			m.stopCronjob(j)
-		}
 	})
 
 	if err != nil {
@@ -114,14 +124,16 @@ func (m *M) createCronjob(j *job.Job) error {
 }
 
 // Method for stopping cronjob when an error is occurred
-func (m *M) stopCronjob(j *job.Job) {
-	j.Status = "error"
-	m.jobstorage.Update(j)
-	m.cron.Stop()
+func (m *M) checkAndStop(err error, j *job.Job) {
+	if err != nil {
+		j.Status = "error"
+		m.jobstorage.Update(j)
+		m.cron.Stop()
+	}
 }
 
+// TODO(nb): V2 create syncrhronizer code for listening to an event
 // Implementation when a listener over events is needed (synchronizer) --> Jobs V2
 func (m *M) createSynchronizer(j *job.Job) error {
-	// TODO(nb): V2 create syncrhronizer code for listening to an event
 	return nil
 }
