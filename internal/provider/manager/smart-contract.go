1a0ebaed0b670a4e94ba0a0082b2d8e626a93ce4
nicobarra <nbarragan01@gmail.com>
2022-11-11 12:56:47 -0300

    refactor(smart-contract.go): contract instance in Call & Perform is now got from bind library and not from abi generated file

--- internal/provider/manager/smart-contract.go
+++ internal/provider/manager/smart-contract.go
@@ -1,7 +1,6 @@
 package providermanager
 
 import (
-	jobsabi "github.com/darchlabs/jobs/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
@@ -11,20 +10,15 @@ import (
 
 func GetContract(address string, inputAbi abi.ABI, backend bind.ContractBackend) *bind.BoundContract {
 	contract := bind.NewBoundContract(common.HexToAddress(address), inputAbi, backend, backend, backend)
+
 	return contract
 }
 
 // Method for calling the smart contract view function with bool return
 func Call(contract *bind.BoundContract, client *ethclient.Client, address string, checkMethod string, opts *bind.CallOpts) (*bool, error) {
-	abiConn, err := jobsabi.NewAbiCaller(common.HexToAddress(address), client)
-	if err != nil {
-		return nil, err
-	}
-
-	abiCaller := jobsabi.AbiCallerRaw{Contract: abiConn}
-
 	var out []interface{}
-	err = abiCaller.Call(opts, &out, checkMethod)
+
+	err := contract.Call(opts, &out, checkMethod)
 	if err != nil {
 		return nil, err
 	}
@@ -35,15 +29,8 @@ func Call(contract *bind.BoundContract, client *ethclient.Client, address string
 
 // Method for calling the smart contract view function with bool return
 func Perform(contract *bind.BoundContract, client *ethclient.Client, address string, actionMethod string, opts *bind.TransactOpts, params interface{}) (*types.Transaction, error) {
-	abiConn, err := jobsabi.NewAbiTransactor(common.HexToAddress(address), client)
-	if err != nil {
-		return nil, err
-	}
-
-	abiTransactor := jobsabi.AbiTransactorRaw{Contract: abiConn}
-
 	if params == nil {
-		tx, err := abiTransactor.Transact(opts, actionMethod)
+		tx, err := contract.Transact(opts, actionMethod)
 		if err != nil {
 			return nil, err
 		}
@@ -51,7 +38,7 @@ func Perform(contract *bind.BoundContract, client *ethclient.Client, address str
 		return tx, nil
 	}
 
-	tx, err := abiTransactor.Transact(opts, actionMethod, params)
+	tx, err := contract.Transact(opts, actionMethod, params)
 	if err != nil {
 		return nil, err
 	}
