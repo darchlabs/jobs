19196b356d8ec1b71a670341fa192c41aec48338
nicobarra <nbarragan01@gmail.com>
2022-12-17 01:23:46 -0300

    refactor(main.go): removed 'node_url' field from config env variables

--- internal/config/main.go
+++ internal/config/main.go
@@ -2,7 +2,6 @@ package config
 
 type Config struct {
 	DatabaseURL string `envconfig:"database_filepath" required:"true"`
-	Port string `envconfig:"port" required:"true"`
-	NodeURL string `envconfig:"node_url" required:"true"`
-	PrivateKey string `envconfig:"private_key" required:"true"`
+	Port        string `envconfig:"port" required:"true"`
+	PrivateKey  string `envconfig:"private_key" required:"true"`
 }
